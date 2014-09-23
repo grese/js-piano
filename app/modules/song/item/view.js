d55268dfce4c15fe5d12aca917475473e887ba98
grese <johngrese@yahoo.com>
2014-09-23 16:24:21 -0700

    added ability to download previously recorded songs.

--- app/modules/song/item/view.js
+++ app/modules/song/item/view.js
@@ -7,8 +7,20 @@ define(function(require, exports, module) {
     var Layout = Backbone.Layout.extend({
         template: require("ldtpl!./template"),
         tagName: "div",
+        downloadFormat: 'mp3',
         serialize: function() {
-            return { model: this.model };
+            var filename = this.model.id + '.' + this.downloadFormat;
+            return {
+                model: this.model,
+                duration: this.msToMinAndSec(this.model.get('duration')),
+                songFile: '/audio/songs/'+filename,
+                downloadFile: filename
+            };
+        },
+        msToMinAndSec: function(millis){
+            var minutes = Math.floor(millis / 60000);
+            var seconds = ((millis % 60000) / 1000).toFixed(0);
+            return minutes + ":" + (seconds < 10 ? '0' : '') + seconds;
         },
         initialize: function() {
             this.listenTo(this.model, "change", this.render);
